cf1bd8ce1d947c0388cf64afadb18ddf8a2957f1
Gitdurgabhavni <durgabhavani33312@gmail.com>
2023-03-30 17:34:33 +0530

    operators

---
+++ Operators.java
@@ -0,0 +1,47 @@
+1.What are the conditional operators in java?
+Ans.In Java, conditional operators check the condition and decides the  result on the basis of both conditions.
+
+2.What are the types of operators based on the number of operands?
+Ans.There are two types of mathematical operators: unary and binary.
+ Unary operators perform an action with a single operand. Binary operators perform actions with two operands.
+
+3.What is the use of Switch case in Java Programming?
+Ans.The switch case in java is used to select one of many code blocks for execution.
+
+4.What are the priority levels of arithmetic operation in Java?
+Ans.Multiplication and division are supposed to take place before addition and subtraction (unless parentheses are used to change the order of operation).
+
+5. What are the conditional Statements and use of conditional statements in Java?
+Ans.Conditional statements define conditions that are true or false and then execute based on whether or not the condition is true.
+Use if to specify a block of code to be executed, if a specified condition is true.
+Use else to specify a block of code to be executed, if the same condition is false.
+Use else if to specify a new condition to test, if the first condition is false.
+
+6. What is the syntax of if-else statement?
+Ans. The syntax of if-else statement is:
+      if(condition){
+          statement - 1
+      }   else {
+          statement - 2
+      }      
+
+7.What are the 3 types of iterative statements in java?
+Ans.For loop
+    While loop
+    Do- While loop
+
+8. Write the difference between for loop and do-while loop?
+Ans.Both for loop and while loop is used to execute the statements repeatedly while the program runs.
+ The major difference between for loop and the while loop is that for loop is used when the number of iterations is known,
+ whereas execution is done in the while loop until the statement in the program is proved wrong.
+
+9. Write a program to print numbers from 1 to 10.
+Ans.public class WhileExample {
+public static void main(String[] args) {
+int i=1;
+while(i<=10){
+System.out.println(i);
+i++;
+}
+}
+}
